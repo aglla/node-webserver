fe046f74d4376004b40ae60825b5a895df07afb6
Ambarish G <a.gugilla@gmail.com>
2018-06-10 15:14:48 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');  
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials")   //for the partial hbs files
+
+hbs.registerHelper('getCurrentYear', function(){      //helper function used in hbs {{getCurrentYear}} templating
+	return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', function(text){
+	return text.toUpperCase();
+})
+
+
+app.set('view engine', 'hbs');
+ 
+
+app.use(function(req, res, next){
+	var now = new Date().toString();
+
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log)
+    fs.appendFile('server.log', log + '\n')
+	next();
+
+})
+
+/*app.use(function(req, res, next){
+  res.render("maintenance.hbs");
+})
+*/
+
+app.use(express.static(__dirname + '/public'));    //middleware
+
+
+app.get('/', function(req, res){
+	//res.send("<h2>hello!</h2>");
+	//res.send({name: "Babbu", likes:['basketball', 'pie']})
+	res.render("home.hbs", {
+		pageTitle: "Home page",
+		welcomeMessage: "This is my site"
+	})
+});
+
+app.get('/about', function(req,res){
+	//res.send("about page")
+	res.render('about.hbs',{
+		pageTitle: "About page"
+	});
+});
+
+app.get('/bad', function(req, res){
+	res.send({error:"not found!"})
+})
+
+
+app.listen(3000, function(){
+	console.log("Server is up on port 3000.")
+});
